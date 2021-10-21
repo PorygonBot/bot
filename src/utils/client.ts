249c4ac3b7c09da44d96abd8823e5f6acca1a78f
harbar20 <harbar2021@gmail.com>
2021-10-21 11:18:57 -0400

    fixed slash command

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,5 +1,11 @@
-import { Client } from "discord.js";
-const client = new Client({ intents: []});
+import { Client, Intents } from "discord.js";
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ]
+});
 
 //This will be used everywhere
 export default client;
