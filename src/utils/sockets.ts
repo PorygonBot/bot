f54db7e995375ec7fac6c1a55a980ecc149cce44
harbar20 <harbar2021@gmail.com>
2024-05-22 22:06:20 -0400

    removed socket servers that don't work anymore

--- src/utils/sockets.ts
+++ src/utils/sockets.ts
@@ -7,59 +7,23 @@ const sockets: { [key: string]: Socket } = {
         ip: "sim3.psim.us:8000",
         server: "ws://sim3.psim.us:8000/showdown/websocket",
     },
-    sports: {
-        name: "Sports",
-        link: "http://sports.psim.us",
-        ip: "54.202.92.100:8000",
-        server: "ws://54.202.92.100:8000/showdown/websocket",
-    },
-    automatthic: {
-        name: "Automatthic",
-        link: "http://automatthic.psim.us",
-        ip: "185.224.89.75:8000",
-        server: "ws://185.224.89.75:8000/showdown/websocket",
-    },
     dawn: {
         name: "Dawn",
         link: "dawn.psim.us",
         ip: "server.dawn-ps.com:8080",
         server: "ws://server.dawn-ps.com:8080/showdown/websocket",
     },
-    drafthub: {
-        name: "Drafthub",
-        link: "http://drafthub.psim.us",
-        ip: "showdown.home.arjunbrar.tech:8000",
-        server: "ws://showdown.home.arjunbrar.tech:8000/showdown/websocket",
-    },
     clover: {
         name: "Clover",
         link: "https://clover.weedl.es",
         ip: "clover.weedl.es:8000",
         server: "ws://clover.weedl.es:8000/showdown/websocket",
-    },
+    }, //THIS COULD BE THE ISSUE
     radicalred: {
         name: "RadicalRed",
-        link: "radicalred.net",
+        link: "https://play.radicalred.net/",
         ip: "sim.radicalred.net:8000",
         server: "ws://sim.radicalred.net:8000/showdown/websocket",
-    },
-    fantasy: {
-        name: "Fantasy",
-        link: "http://fantasyclient.herokuapp.com/",
-        ip: "fantasy-showdown.herokuapp.com:8000",
-        server: "ws://fantasy-showdown.herokuapp.com:8000/showdown/websocket",
-    },
-    legacyvgc: {
-        name: "Legacy VGC",
-        link: "http://legacyvgc.psim.us/",
-        ip: "35.175.128.48:8000",
-        server: "ws://35.175.128.48:8000/showdown/websocket",
-    },
-    dragonheaven: {
-        name: "Showdown",
-        link: "https://dragonheaven.psim.us/",
-        ip: "sim3.psim.us:8000",
-        server: "ws://sim3.psim.us:8000/showdown/websocket",
     },
     dl: {
         name: "DL",
