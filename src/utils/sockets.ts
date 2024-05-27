63dd0707ed9b28d2e715836c4f5112424ef3586d
harbar20 <harbar2021@gmail.com>
2024-05-26 23:44:14 -0400

    removed clover

--- src/utils/sockets.ts
+++ src/utils/sockets.ts
@@ -13,12 +13,6 @@ const sockets: { [key: string]: Socket } = {
         ip: "server.dawn-ps.com:8080",
         server: "ws://server.dawn-ps.com:8080/showdown/websocket",
     },
-    clover: {
-        name: "Clover",
-        link: "https://clover.weedl.es",
-        ip: "clover.weedl.es:8000",
-        server: "ws://clover.weedl.es:8000/showdown/websocket",
-    }, //THIS COULD BE THE ISSUE
     radicalred: {
         name: "RadicalRed",
         link: "https://play.radicalred.net/",
